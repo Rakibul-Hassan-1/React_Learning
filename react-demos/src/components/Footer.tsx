16965a37cf9b39ea3c198ff787280c20d1fdada5
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2025-01-12 14:26:42 +0600

    first react app

---
+++ react-demos/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <h1>Footer Section</h1>
+      <p>© 2025 My Website</p>
+    </footer>
+  );
+};
+
+export default Footer;
