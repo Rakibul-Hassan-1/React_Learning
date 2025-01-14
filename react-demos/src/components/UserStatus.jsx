9afa2529cbf30377ea2a44dfa56b6dc70d24a44f
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2025-01-14 12:34:34 +0600

    style added dynamically

--- react-demos/src/components/UserStatus.jsx
+++ react-demos/src/components/UserStatus.jsx
@@ -1,18 +1,37 @@
+// const UserStatus = (props) => {
+//   if (props.loogedIn) {
+//     if (props.isAdmin) {
+//       return (
+//         <h1
+//           style={{
+//             color: "white",
+//             backgroundColor: "teal",
+//             padding: "3rem",
+//             fontSize: "3rem",
+//           }}
+//         >
+//           Welcome Admin
+//         </h1>
+//       );
+//     } else {
+//       return <h1>Welcome User</h1>;
+//     }
+//   } else {
+//     return <h1>Please Login</h1>;
+//   }
+// };
+// export default UserStatus;
+
 const UserStatus = (props) => {
+  const style = {
+    color: "white",
+    backgroundColor: "teal",
+    padding: "3rem",
+    fontSize: "3rem",
+  };
   if (props.loogedIn) {
     if (props.isAdmin) {
-      return (
-        <h1
-          style={{
-            color: "white",
-            backgroundColor: "teal",
-            padding: "3rem",
-            fontSize: "3rem",
-          }}
-        >
-          Welcome Admin
-        </h1>
-      );
+      return <h1 style={style}>Welcome Admin</h1>;
     } else {
       return <h1>Welcome User</h1>;
     }
