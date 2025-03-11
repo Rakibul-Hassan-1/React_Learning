1f9049aede67ed3658e53dbcbbf3f370be7ab3a2
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2025-03-11 22:25:03 +0600

    Counter.jsx are added

---
+++ e_com/src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  return (
+    <div className="container">
+      <div>
+        <h1 className="number">{count}</h1>
+      </div>
+      <div className="btn-container">
+        <button onClick={increment} className="increment">
+          +
+        </button>
+        <button onClick={decrement} className="decrement">
+          -
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
